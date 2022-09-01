59f8514015d868d0cfe3bc5cff5c39eb76c79e19
Giuseppe Antonio Nanna <g.a.nanna@inventati.org>
2022-09-01 17:41:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-# rm -r ~/.cache/thumbnails
-
 cp *.thumbnailer /usr/share/thumbnailers/
 cp *-thumbnailer-script /usr/local/bin/
-#xdg-mime install --novendor stl-mime.xml
-#xdg-mime install --novendor scad-mime.xml
-#update-mime-database /usr/share/mime
+
+xdg-mime install --novendor stl-mime.xml
+xdg-mime install --novendor scad-mime.xml
+update-mime-database /usr/share/mime
